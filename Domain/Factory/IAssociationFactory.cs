2fc482e1ca76e0ceb31ba9a67048060535ba3547
ines.garcia <ines.garcia@b-simple.pt>
2024-04-09 15:59:48 +0100

    fixed gitignore

--- Domain/Factory/IAssociationFactory.cs
+++
@@ -1,9 +0,0 @@
-using Domain.Model;
-
-namespace Domain.Factory
-{
-    public interface IAssociationFactory
-    {
-        Association NewAssociation(long colaboratorId, long projectId,DateOnly periodStart, DateOnly periodEnd);
-    }
-}
