d53d0ac7c92e7ca63f00a17e3bc83b43f81881b7
Henrique Pereira <henrique.pereira@b-simple.pt>
2024-04-11 14:39:12 +0100

    Refactor publish

--- Gateway/AssociationAmqpGateway.cs
+++ Gateway/AssociationAmqpGateway.cs
@@ -5,24 +5,24 @@ using RabbitMQ.Client;
 
 public class AssociationAmqpGateway
 {
+    private readonly ConnectionFactory _factory;
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
     public AssociationAmqpGateway()
     {
+        _factory = new ConnectionFactory { HostName = "localhost" };
+        _connection = _factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
     }
 
     public void Publish()
     {
-        var factory = new ConnectionFactory { HostName = "localhost" };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
-
-        channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
-
         var message = "hello world";
         var body = Encoding.UTF8.GetBytes(message);
-        channel.BasicPublish(exchange: "logs",
+        _channel.BasicPublish(exchange: "logs",
                              routingKey: string.Empty,
                              basicProperties: null,
                              body: body);
-
     }
 }
