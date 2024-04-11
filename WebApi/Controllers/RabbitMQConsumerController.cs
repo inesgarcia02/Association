77a4f0c1a29417a393f542ef54f53d626942306d
ines.garcia <ines.garcia@b-simple.pt>
2024-04-11 14:39:01 +0100

    some comments

--- WebApi/Controllers/RabbitMQConsumerController.cs
+++ WebApi/Controllers/RabbitMQConsumerController.cs
@@ -15,24 +15,32 @@ namespace WebApi.Controllers
             _factory = new ConnectionFactory { HostName = "localhost" };
             _connection = _factory.CreateConnection();
             _channel = _connection.CreateModel();
-        }
 
-        public void StartConsuming()
-        {            
+            //_channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
             _channel.QueueDeclare(queue: "hello",
-                                durable: false,
+                                durable: true,
                                 exclusive: false,
                                 autoDelete: false,
                                 arguments: null);
+            
+            // _channel.QueueBind(queue: "hello",
+            //       exchange: "logs",
+            //       routingKey: string.Empty);
 
             Console.WriteLine(" [*] Waiting for messages.");
+        }
 
+        public void StartConsuming()
+        {         
             var consumer = new EventingBasicConsumer(_channel);
             consumer.Received += (model, ea) =>
             {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine($" [x] Received {message}");
+
+                _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
             _channel.BasicConsume(queue: "hello",
                                 autoAck: true,
