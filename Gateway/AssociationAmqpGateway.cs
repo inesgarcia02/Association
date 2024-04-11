eba0651eb4f2a0e4587c323b441f62d9f2a8c442
Henrique Pereira <henrique.pereira@b-simple.pt>
2024-04-11 12:57:40 +0100

    amqp gw added

---
+++ Gateway/AssociationAmqpGateway.cs
@@ -0,0 +1,28 @@
+﻿namespace Gateway;
+using System.Text;
+using RabbitMQ.Client;
+
+
+public class AssociationAmqpGateway
+{
+    public AssociationAmqpGateway()
+    {
+    }
+
+    public void Publish()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+        var message = "hello world";
+        var body = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(exchange: "logs",
+                             routingKey: string.Empty,
+                             basicProperties: null,
+                             body: body);
+
+    }
+}
